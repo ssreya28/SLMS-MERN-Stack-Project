395c33e789e61ba313b9b1708fa699d50c45612b
Sreya Srungarapu <110274619+ssreya28@users.noreply.github.com>
2023-07-27 15:27:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -28,8 +29,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.use('*', function(req, res){
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
 });
 
 //PORT

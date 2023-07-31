9e92462201edfe3beda9d4710e924ec1a522ffb0
Sreya Srungarapu <110274619+ssreya28@users.noreply.github.com>
2023-07-31 14:00:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
